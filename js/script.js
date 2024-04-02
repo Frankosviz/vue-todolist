5112290e9fb9404ce864a89052e2d9a2ebfbcc11
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-03 00:21:52 +0200

    Todolist VueJs - Remove item function done

--- js/script.js
+++ js/script.js
@@ -11,14 +11,23 @@ createApp({
         }
     },
     methods: {
-        toggleAcquistato(id){
-            const ingrediente = this.ingredienti.find((el)=>{
+        // Creo la funzione per sbarrare gli ingredienti che sono stati gia ordinati/acquistati
+        toggleAcquistato(id) {
+            const ingrediente = this.ingredienti.find((el) => {
                 return el.id === id;
             })
             console.log(ingrediente);
-            if(ingrediente){
-            ingrediente.acquistato = !ingrediente.acquistato;
-        }
-    }
+            if (ingrediente) {
+                ingrediente.acquistato = !ingrediente.acquistato;
+            }
+        },
+        removeItem(id) {
+            const i = this.ingredienti.findIndex((el) => {
+                return el.id === id
+            })
+            if (i !== -1) { //questo è il controllo per vedere se non è meno1 per eliminarlo con splice
+                this.ingredienti.splice(i, 1);
+            }
+        },
     }
 }).mount('#app')
