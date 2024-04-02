6a731c665ae27734248ff7b0b7e95e0ca8f7d5d6
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-03 00:12:29 +0200

    Todolist VueJs - Coding

--- js/data.js
+++ js/data.js
@@ -23,4 +23,125 @@ export const ingredienti = [
         id: 5,
         text: "Carciofi",
         acquistato: false
-    }];
+    },
+    {
+        id: 6,
+        text: "Funghi",
+        acquistato: false
+    },
+    {
+        id: 7,
+        text: "Olive",
+        acquistato: false
+    },
+    {
+        id: 8,
+        text: "Crema di pistacchio",
+        acquistato: false
+    },
+    {
+        id: 9,
+        text: "Mortadella",
+        acquistato: false
+    },
+    {
+        id: 10,
+        text: "Salame piccante",
+        acquistato: false
+    },
+    {
+        id: 11,
+        text: "Pancetta",
+        acquistato: false
+    },
+    {
+        id: 12,
+        text: "Prosciutto Crudo",
+        acquistato: false
+    },
+    {
+        id: 13,
+        text: "Capperi",
+        acquistato: false
+    },
+    {
+        id: 14,
+        text: "Alici",
+        acquistato: false
+    },
+    {
+        id: 15,
+        text: "Tonno",
+        acquistato: false
+    },
+    {
+        id: 16,
+        text: "Peperoni",
+        acquistato: false
+    },
+    {
+        id: 17,
+        text: "Cipolle",
+        acquistato: false
+    },
+    {
+        id: 18,
+        text: "Cetrioli",
+        acquistato: false
+    },
+    {
+        id: 19,
+        text: "Peperoncino",
+        acquistato: false
+    },
+    {
+        id: 20,
+        text: "Friarielli",
+        acquistato: false
+    },
+    {
+        id: 21,
+        text: "Farina integrale",
+        acquistato: false
+    },
+    {
+        id: 22,
+        text: "Farina bianca",
+        acquistato: false
+    },
+    {
+        id: 23,
+        text: "Origano",
+        acquistato: false
+    },
+    {
+        id: 24,
+        text: "Mozzarela",
+        acquistato: false
+    },
+    {
+        id: 25,
+        text: "Basilico",
+        acquistato: false
+    },
+    {
+        id: 26,
+        text: "Burrata",
+        acquistato: false
+    },  
+    {
+        id: 27,
+        text: "Funghi porcini",
+        acquistato: false
+    },
+    {
+        id: 28,
+        text: "Scarola",
+        acquistato: false
+    },
+    {
+        id: 29,
+        text: "Rucola",
+        acquistato: false
+    }
+];
